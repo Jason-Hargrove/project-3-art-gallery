41878a521e44ec2fa4316b24ec44eb1c11107b8b
Jason Hargrove <rijaha@gmail.com>
2021-08-18 13:18:00 -0500

    Able to search API with form and link.

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,15 +2,42 @@ import React, { useState } from 'react';
 
 export default function App(props) {
 	const [name, updateName] = useState('Jason');
-	const [query, updateQuery] useState({
-		baseUrl: 'https://collectionapi.metmuseum.org/public/collection/v1/objects/',
-		option: '&t=',
+	const [query, updateQuery] = useState({
+		baseURL: 'https://collectionapi.metmuseum.org/public/collection/v1/search?',
+		option: 'title=true&q=',
 		title: '',
 		searchURL: ''
-		
-	})
+	});
 
+	const handleChange = e => {
+		updateQuery({ ...query, ...{ [e.target.id]: e.target.value } });
+	};
 
+	const handleSubmit = e => {
+		e.preventDefault();
+		updateQuery({
+			...query,
+			searchURL: query.baseURL + query.option + query.title
+		});
+	};
 
-	return <div className="AppPage">This is the {props.page} page</div>;
+	return (
+		<div className="AppPage">
+			<h2>Gallery App</h2>
+			<h3>{name}</h3>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="title"> Title</label>
+				<input
+					id="title"
+					type="text"
+					value={query.title}
+					onChange={handleChange}
+				/>
+				<input type="submit" value="Find Art" />
+			</form>
+			<h1>
+				<a href={query.searchURL}>Your URL:{query.searchURL}</a>
+			</h1>
+		</div>
+	);
 }
