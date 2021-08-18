026d18b97807854669d31f92ff1df3fc02593560
Jason Hargrove <rijaha@gmail.com>
2021-08-17 22:11:26 -0500

    Working on api.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
 
 export default function App(props) {
+	const [name, updateName] = useState('Jason');
+	const [query, updateQuery] useState({
+		baseUrl: 'https://collectionapi.metmuseum.org/public/collection/v1/objects/',
+		option: '&t=',
+		title: '',
+		searchURL: ''
+		
+	})
+
+
+
 	return <div className="AppPage">This is the {props.page} page</div>;
 }
