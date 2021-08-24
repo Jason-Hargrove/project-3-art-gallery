231808bd75a4b97c08434211120860330492c7ba
Jason Hargrove <rijaha@gmail.com>
2021-08-24 13:26:57 -0500

    Routes written. Begin testing.

--- controllers/ads.js
+++ controllers/ads.js
@@ -2,6 +2,16 @@ const Ad = require('../models/blog');
 const router = require('express').Router();
 
 // ===== Create =====
+router.post('/', async(req, res) => {
+  try {
+    const createdAd = await Ad.create(req.body)
+    res.status(200).json(createdAd)
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({message: error.message})
+  }
+})
+
 
 // ===== Read =====
   // Index
@@ -38,3 +48,14 @@ router.put('/:id', async (req, res) => {
 })
 
 // ===== Delete =====
+router.delete('/:id', async(req,res) => {
+  try {
+    const deletedAd = await Ad.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedAd)
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({message: error.message})
+  }
+})
+
+module.exports = router;
